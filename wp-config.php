957ef99cc067697657f684fb5ca112986be6d1aa
Simon <33090598+SoleauSimon@users.noreply.github.com>
2018-05-25 10:53:59 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,13 +22,13 @@
 define('DB_NAME', 'tpColorWordpress');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'yourpassword');
+define('DB_PASSWORD', '');
 
 /** Adresse de l’hébergement MySQL. */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8mb4');
